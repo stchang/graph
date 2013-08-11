3c719837bedf70a9f12c62b3d4c99de82c792aa8
Stephen Chang <stchang216@gmail.com>
2013-08-11 03:05:56 -0400

    add run-all-tests file

---
+++ graph/tests/run-all-tests.rkt
@@ -0,0 +1,6 @@
+#lang racket
+
+(require "graph-fns-basic-tests.rkt"
+         "graph-fns-minspantree-tests.rkt"
+         "graph-fns-singlesource-shortestpaths-tests.rkt"
+         "graph-fns-allpairs-shortestpaths-tests.rkt")
