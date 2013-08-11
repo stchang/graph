6a8c17afd359eb986195b823c0e1e362997e68e8
Stephen Chang <stchang216@gmail.com>
2013-08-11 01:37:01 -0400

    add priority queue

---
+++ queue/priority.rkt
@@ -0,0 +1,24 @@
+#lang racket
+(require "gen-queue.rkt")
+(require (prefix-in r: data/heap))
+(provide (all-defined-out))
+
+;; priority queue such that on dequeue of element x,
+;; pop all copies of x from the queue
+
+(struct priority (elements)
+  #:methods gen:queue
+  [(define (enqueue! pq x) (r:heap-add! (priority-elements pq) x))
+   (define (peek pq) (r:heap-min (priority-elements pq)))
+   (define (dequeue! pq) 
+     (define hp (priority-elements pq))
+     (define x (r:heap-min hp))
+     (begin0 x
+             (let loop ()
+               (r:heap-remove-min! hp)
+               (when (and (not (zero? (r:heap-count hp))) 
+                          (equal? (r:heap-min hp) x))
+                 (loop)))))
+   (define (empty? pq) (zero? (r:heap-count (priority-elements pq))))])
+     
+(define (mk-empty-priority <=) (priority (r:make-heap <=)))
