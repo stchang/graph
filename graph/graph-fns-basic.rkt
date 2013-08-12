2661faa51266ea0987a87fe5076e96c2b42cf67c
Stephen Chang <stchang216@gmail.com>
2013-08-12 02:16:16 -0400

    refactor dfs/generic: pro/epilogue
    - refactor scc to use dfs/generic

--- graph/graph-fns-basic.rkt
+++ graph/graph-fns-basic.rkt
@@ -103,128 +103,223 @@
   (define time 0)
   
   (define (init G)
-    (for ([u (in-vertices G)]) (color-set! u WHITE) (π-set! u #f)))
+    (for ([u (in-vertices G)]) (color-set! u WHITE) #;(π-set! u #f)))
   
-  (define (visit? G u) (white? (color u)))
+  (define (visit? G parent u) (white? (color u)))
   
-  (define (pre-visit u) (color-set! u GRAY) (add1! time) (d-set! u time))
+  (define (prologue G parent u) 
+    (color-set! u GRAY) (add1! time) (d-set! u time)
+    (π-set! u parent))
   
-  (define (process-neighbor? G u v) (white? (color v)))
-
-  (define (process-neighbor G u v) (π-set! v u))
-  
-  (define (post-visit u) (color-set! u BLACK) (add1! time) (f-set! u time))
+  (define (epilogue G parent u)
+    (color-set! u BLACK) (add1! time) (f-set! u time))
   
   (define (finish G) (values color d π f))
   
   (dfs/generic G #:init init
                  #:visit? visit?
-                 #:pre-visit pre-visit
-                 #:process-neighbor? process-neighbor?
-                 #:process-neighbor process-neighbor
-                 #:post-visit post-visit
+                 #:prologue prologue
+                 #:epilogue epilogue
                  #:return finish))
 
 (define (dfs/generic G #:order [order (λ (vs) vs)]
                        #:break [break? (λ _ #f)]
                        #:init [init void]
                        #:visit? [visit? (λ _ #t)]
-                       #:pre-visit [pre-visit void]
-                       #:process-neighbor? [process-neighbor? (λ _ #f)]
-                       #:process-neighbor [process-neighbor void]
-                       #:post-visit [post-visit void]
+                       #:prologue [prologue void]
+                       #:epilogue [epilogue void]
+                       #:process-unvisited? [process-unvisited? (λ _ #f)]
+                       #:process-unvisited [process-unvisited void]
                        #:return [finish void])
   (init G)
-  (for ([u (order (in-vertices G))] #:when (visit? G u) #:break (break?))
-    (let dfs-visit ([u u])
-      (pre-visit u)
-      (for ([v (in-neighbors G u)] 
-            #:when (process-neighbor? G u v) #:break (break?))
-        (process-neighbor G u v)
-        (dfs-visit v))
-      (post-visit u)))
+  
+;  (define (do-visit u)
+;    (pre-visit u)
+;    (for ([v (in-neighbors G u)] 
+;          #:when (process-neighbor? G u v) #:break (break?))
+;      (process-neighbor G u v)
+;      (do-visit v))
+;    (post-visit u))
+;
+;  (for ([u (order (in-vertices G))] #:when (visit? G u) #:break (break?)) (do-visit u))
+
+  ;; inner loop: keep following (unvisited) links
+  (define (do-visit parent u)
+    (prologue G parent u)
+    (for ([v (in-neighbors G u)] #:break (break?))
+      (cond [(visit? G u v) (do-visit u v)]
+            [(process-unvisited? G u v) (process-unvisited G u v)]))
+    (epilogue G parent u))
+  
+  ;; outer loop: picks a new start node when previous search reaches dead end
+  (for ([u (order (in-vertices G))] #:break (break?))
+    (cond [(visit? G #f u) (do-visit #f u)]
+          [(process-unvisited? G #f u) (process-unvisited G #f u)]))
+
   (finish G))
 
 (define (dag? G)
   (define-hashes color)
   (define not-dag #f)
-  
+
   (define (init G) (for ([u (in-vertices G)]) (color-set! u WHITE)))
   
-  (define (visit? G u) (white? (color u)))
+  (define (visit? G parent u) (white? (color u)))
   
-  (define (pre-visit u) (color-set! u GRAY))
+  (define (prologue G parent u) (color-set! u GRAY))
   
-  (define (process-neighbor? G u v) 
-    (if (gray? (color v)) 
-        (begin0 #f (set! not-dag #t))
-        (not (black? (color v)))))
+  (define (process-unvisited? G parent v) (gray? (color v)))
+  (define (process-unvisited G parent v) (set! not-dag #t))
   
   (define (not-dag?) not-dag)
   
-  (define (post-visit u) (color-set! u BLACK))
+  (define (epilogue G parent u) (color-set! u BLACK))
   
   (define (finish G) (not not-dag))
     
   (dfs/generic G #:break not-dag?
                  #:init init
                  #:visit? visit?
-                 #:pre-visit pre-visit
-                 #:process-neighbor? process-neighbor?
-                 #:post-visit post-visit
+                 #:prologue prologue
+                 #:epilogue epilogue
+                 #:process-unvisited? process-unvisited?
+                 #:process-unvisited process-unvisited
                  #:return finish))
+
+;  (define (init G) (for ([u (in-vertices G)]) (color-set! u WHITE)))
+;  
+;  (define (visit? G u) (white? (color u)))
+;  
+;  (define (pre-visit u) (color-set! u GRAY))
+;  
+;  (define (process-neighbor? G u v) 
+;    (if (gray? (color v)) 
+;        (begin0 #f (set! not-dag #t))
+;        (not (black? (color v)))))
+;  
+;  (define (not-dag?) not-dag)
+;  
+;  (define (post-visit u) (color-set! u BLACK))
+;  
+;  (define (finish G) (not not-dag))
+;    
+;  (dfs/generic G #:break not-dag?
+;                 #:init init
+;                 #:visit? visit?
+;                 #:pre-visit pre-visit
+;                 #:process-neighbor? process-neighbor?
+;                 #:post-visit post-visit
+;                 #:return finish))
 
 (define (tsort G)
   (define res null) (define (add-finished! v) (set! res (cons v res)))
   (define-hashes color)
-  
+
   (define (init G) (for ([u (in-vertices G)]) (color-set! u WHITE)))
   
-  (define (visit? G u) (white? (color u)))
+  (define (visit? G parent u) (white? (color u)))
   
-  (define (pre-visit u) (color-set! u GRAY))
+  (define (prologue G parent u) (color-set! u GRAY))
   
-  (define (process-neighbor? G u v) (white? (color v)))
+;  (define (process-neighbor? G u v) (white? (color v)))
 
-  (define (post-visit u) (color-set! u BLACK) (add-finished! u))
+  (define (epilogue G parent u) (color-set! u BLACK) (add-finished! u))
   
   (define (finish G) res)
   
   (dfs/generic G #:init init
                  #:visit? visit?
-                 #:pre-visit pre-visit
-                 #:process-neighbor? process-neighbor?
-                 #:post-visit post-visit
+                 #:prologue prologue
+                 #:epilogue epilogue
                  #:return finish))
-
-;  (define-values (color d π f) (dfs G))
-;  (sort (hash-keys f) > #:key (λ (k) (hash-ref f k))))
+;  (define (init G) (for ([u (in-vertices G)]) (color-set! u WHITE)))
+;  
+;  (define (visit? G u) (white? (color u)))
+;  
+;  (define (pre-visit u) (color-set! u GRAY))
+;  
+;  (define (process-neighbor? G u v) (white? (color v)))
+;
+;  (define (post-visit u) (color-set! u BLACK) (add-finished! u))
+;  
+;  (define (finish G) res)
+;  
+;  (dfs/generic G #:init init
+;                 #:visit? visit?
+;                 #:pre-visit pre-visit
+;                 #:process-neighbor? process-neighbor?
+;                 #:post-visit post-visit
+;                 #:return finish))
 
   
 ;; tarjan algorithm for strongly connected components
 (define (scc G)
   (define i 0)
   (define-hashes index lowlink)
-  (define S null)
+
+  (define S null)   
   (define (S-push x) (set! S (cons x S)))
   
   (define SCC null)
+  (define (build-SCC? v) (= (lowlink v) (index v)))
+  (define (build-SCC v)
+    (define-values (new-scc S-rst) (splitf-at S (λ (w) (not (equal? w v)))))
+    (set! SCC (cons (cons v new-scc) SCC))
+    (set! S (cdr S-rst)))
   
-  (define (strongconnect v)
-    (index-set! v i) (lowlink-set! v i) (add1! i) (S-push v)
-    
-    (for ([w (in-neighbors G v)])
-      (cond [(not (hash-has-key? index w))
-             (strongconnect w)
-             (lowlink-set! v (min (lowlink v) (lowlink w)))]
-            [(member w S)
-             (lowlink-set! v (min (lowlink v) (index w)))]))
-    
-    (when (= (lowlink v) (index v)) ;; v is root node, so create an SCC
-      (define-values (new-scc S-rst) (splitf-at S (λ (w) (not (equal? w v)))))
-      (set! SCC (cons (cons v new-scc) SCC))
-      (set! S (cdr S-rst))))
+  (define (visit? G v to-visit) (not (hash-has-key? index to-visit)))
   
-  (for ([v (in-vertices G)] #:unless (hash-has-key? index v)) (strongconnect v))
+  (define (prologue G parent v)
+    (index-set! v i) (lowlink-set! v i) (add1! i) (S-push v))
+
+  ;(define (process-neighbor? G v w) (not (hash-has-key? index w)))
+
+  (define (process-unvisited? G v unvisited) (member unvisited S))
+  (define (process-unvisited G v unvisited)
+    (lowlink-set! v (min (lowlink v) (index unvisited))))
   
-  SCC)
+  (define (epilogue G parent v)
+    (when (build-SCC? v) (build-SCC v))
+    (when parent 
+      (lowlink-set! parent (min (lowlink parent) (lowlink v)))))
+  
+  (define (finish G) SCC)
+  
+  (dfs/generic G #:visit? visit?
+                 #:prologue prologue
+                 #:epilogue epilogue
+                 #:process-unvisited? process-unvisited?
+                 #:process-unvisited process-unvisited
+                 #:return finish))
+  
+;  (define (strongconnect v)
+;    (printf "strongconnect ~a\n" v)
+;    (index-set! v i) (lowlink-set! v i) (add1! i) (S-push v)
+;    (printf "indices ~a\n" index)
+;    (printf "lowlinks ~a\n" lowlink)
+;    (printf "S ~a\n" S)
+;    
+;    (for ([w (in-neighbors G v)])
+;      (printf "checking edge ~a ~a\n" v w)
+;      (cond [(not (hash-has-key? index w))
+;             (strongconnect w)
+;             (printf "edge ~a ~a RETURN\n" v w)
+;             (printf "SET ~a lowlink from ~a to " v (lowlink v))
+;             (lowlink-set! v (min (lowlink v) (lowlink w)))
+;             (printf "~a\n" (lowlink v))]
+;            [(member w S)
+;             (printf "SET ~a lowlink from ~a to " v (lowlink v))
+;             (lowlink-set! v (min (lowlink v) (index w)))
+;             (printf "~a\n" (lowlink v))]))
+;    (printf "~a DONE CHECKING NEIGHBORS\n" v)
+;    (printf "indices ~a\n" index)
+;    (printf "lowlinks ~a\n" lowlink)
+;    (when (= (lowlink v) (index v)) ;; v is root node, so create an SCC
+;      (define-values (new-scc S-rst) (splitf-at S (λ (w) (not (equal? w v)))))
+;      (printf "POPPING ~a\n" (cons v new-scc))
+;      (set! SCC (cons (cons v new-scc) SCC))
+;      (set! S (cdr S-rst))))
+;  
+;  (for ([v (in-vertices G)] #:unless (hash-has-key? index v)) (strongconnect v))
+;  
+;  SCC)
