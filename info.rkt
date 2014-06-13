8335ff93acb29190c48c57aab17ab1206195eadd
Stephen Chang <stchang216@gmail.com>
2014-06-13 13:03:15 -0400

    update build-deps to include data-doc

--- info.rkt
+++ info.rkt
@@ -1,4 +1,4 @@
 #lang setup/infotab
 (define collection 'multi)
 (define deps '("base" "data-lib"))
-(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib"))
+(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib" "data-doc"))
