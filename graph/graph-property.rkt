357b052ca3e2691d84a14e2f4e842c078dac9d65
Stephen Chang <stchang216@gmail.com>
2014-06-26 19:19:45 -0400

    add define-edge-property

--- graph/graph-property.rkt
+++ graph/graph-property.rkt
@@ -7,11 +7,14 @@
 
 (provide (all-defined-out))
 
+;; TODO: make vertex and edge properties higher order?
+
 (define-syntax-parameter $v (syntax-rules ()))
 
 (define-syntax (define-vertex-property stx)
   (syntax-parse stx
-    [(_ g prop (~optional (~seq #:init init-val)))
+    [(_ g prop (~or (~optional (~seq #:init init-expr:expr))
+                    (~optional (~seq #:vs vs))) ...)
      (with-syntax 
        ([hash-name (generate-temporary #'prop)]
         [hash-name-set! (format-id #'prop "~a-set!" #'prop)])
@@ -22,13 +25,45 @@
             (syntax-id-rules (set!)
               [(set! prop new-h) (set! hash-name new-h)]
               [(_ key) (hash-ref hash-name key)]
-              [(_ key fail) (hash-ref hash-name key (thunk fail))]
+              [(_ key #:default fail) (hash-ref hash-name key (thunk fail))]
               [prop hash-name]))
           (define (hash-name-set! key val) (hash-set! hash-name key val))
-          (?? (for ([v (in-vertices g)]) 
+          (?? (for ([v (?? vs (in-vertices g))])
                 (hash-name-set! v 
                   (syntax-parameterize ([$v (syntax-id-rules () [_ v])])
-                    init-val)))))))]))
+                    init-expr)))))))]))
+
+(define-syntax-parameter $from (syntax-rules ()))
+(define-syntax-parameter $to (syntax-rules ()))
+
+(define-syntax (define-edge-property stx)
+  (syntax-parse stx
+    [(_ g prop (~or (~optional (~seq #:init init-val:expr))
+                    (~optional (~seq #:for-each init-expr:expr ...))) ...)
+     (with-syntax 
+       ([hash-name (generate-temporary #'prop)]
+        [hash-name-set! (format-id #'prop "~a-set!" #'prop)])
+       (template
+        (begin
+          (define hash-name (make-hash))
+          (define-syntax prop
+            (syntax-id-rules (set!)
+              [(set! prop new-h) (set! hash-name new-h)]
+              [(_ u v) (hash-ref hash-name (list u v))]
+              [(_ u v #:default fail) (hash-ref hash-name (list u v) (thunk fail))]
+              [prop hash-name]))
+          (define (hash-name-set! u v val) (hash-set! hash-name (list u v) val))
+          (?? (let ([vs (get-vertices g)])
+                (for* ([i vs] [j vs])
+                (hash-name-set! i j 
+                  (syntax-parameterize ([$from (syntax-id-rules () [_ i])]
+                                        [$to (syntax-id-rules () [_ j])])
+                    init-val)))))
+          (?? (let ([vs (get-vertices g)])
+                (for* ([i vs] [j vs])
+                  (syntax-parameterize ([$from (syntax-id-rules () [_ i])]
+                                        [$to (syntax-id-rules () [_ j])])
+                    init-expr ...)))))))]))
 
 (define-syntax-rule (define-vertex-properties g p ...) 
   (begin (define-vertex-property g p) ...))
