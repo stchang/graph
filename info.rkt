3fecadfacde3e8fd18fc56328f59b26a2546441a
Stephen Chang <stchang@ccs.neu.edu>
2014-07-01 17:12:39 -0400

    add math-lib dep to info.rkt

--- info.rkt
+++ info.rkt
@@ -1,4 +1,5 @@
 #lang setup/infotab
 (define collection 'multi)
-(define deps '("base" "data-lib"))
-(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib" "data-doc"))
+(define deps '("base" "data-lib" "math-lib"))
+(define build-deps 
+  '("rackunit-lib" "racket-doc" "scribble-lib" "data-doc" "math-doc"))
