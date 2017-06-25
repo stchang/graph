5c068e1d41a0ba9780418bedf92b41e67a8a11df
Leif Andersen <leif@leifandersen.net>
2017-06-25 15:51:46 -0400

    Sanatize the graphviz node names.

--- graph/graph-fns-graphviz.rkt
+++ graph/graph-fns-graphviz.rkt
@@ -8,23 +8,43 @@
 (define-syntax-rule (first x) (unsafe-car x))
 (define-syntax-rule (second x) (unsafe-car (unsafe-cdr x)))
 
+(define (sanatize-name name)
+  (cond
+    [(string? name) (with-output-to-string
+                        (λ () (write name)))]
+    [(symbol? name) (sanatize-name (symbol->string name))]
+    [(number? name) (sanatize-name (number->string name))]
+    [else (sanatize-name
+           (with-output-to-string
+               (λ () (display name))))]))
+
 ;; Return a graphviz definition for a graph
 ;; Pass a hash of vertex -> exact-nonnegative-integer? as coloring to color the nodes
 (define (graphviz g #:colors [colors #f])
   (with-output-to-string
    (λ ()
     (define weighted? (weighted-graph? g))
+    (define node-count 0)
+    (define node-id-table (make-hash))
+    (define (node-id-table-ref! node)
+      (hash-ref! node-id-table node
+                 (λ ()
+                   (begin0 (format "node~a" node-count)
+                           (set! node-count (add1 node-count))))))
     (printf "digraph G {\n")
     ; Add vertices, color them using evenly spaced HSV colors if given colors
     (define color-count (and colors (add1 (apply max (hash-values colors)))))
     (for ([v (in-vertices g)])
       (cond
         [(and color-count (hash-ref colors v #f))
-         (printf "\t~a [color=\"~a 1.0 1.0\"];\n" v 
+         (printf "\t~a [label=~a,color=\"~a 1.0 1.0\"];\n" (node-id-table-ref! v)
+                 (sanatize-name v)
                  (~a #:max-width 5
                      (exact->inexact (/ (hash-ref colors v #f) color-count))))]
         [else
-         (printf "\t~a;\n" v)]))
+         (printf "\t~a [label=~a];\n"
+                 (node-id-table-ref! v)
+                 (sanatize-name v))]))
         
     ; Write undirected edges as one subgraph
     (printf "\tsubgraph U {\n")
@@ -37,9 +57,9 @@
                              (= (edge-weight g (first e) (second e))
                                 (edge-weight g (second e) (first e)))))
         (printf "\t\t~a -> ~a~a;\n" 
-          (first e) 
-          (second e) 
-          (if weighted? (format " [label=~a]" (edge-weight g (first e) (second e))) ""))
+          (node-id-table-ref! (first e))
+          (node-id-table-ref! (second e))
+          (if weighted? (format " [label=\"~a\"]" (edge-weight g (first e) (second e))) ""))
         (set-add (set-add added e) (list (second e) (first e)))))
     (printf "\t}\n")
         
@@ -47,9 +67,9 @@
     (printf "\tsubgraph D {\n")
     (for ([e (in-edges g)] #:unless (set-member? undirected-edges e))
       (printf "\t\t~a -> ~a~a;\n" 
-        (first e) 
-        (second e) 
-        (if weighted? (format " [label=~a]" (edge-weight g (first e) (second e))) "")))
+        (node-id-table-ref! (first e))
+        (node-id-table-ref! (second e))
+        (if weighted? (format " [label=\"~a\"]" (edge-weight g (first e) (second e))) "")))
     (printf "\t}\n")
     
     (printf "}\n"))))
