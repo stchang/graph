f4550e334ac345f726ffbddec55153fe0e25ed30
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-12-12 09:59:53 +0800

    graph-lib graph-doc: Add missing dependencies

--- graph-doc/info.rkt
+++ graph-doc/info.rkt
@@ -7,6 +7,6 @@
 
 (define deps '(("racket" "5.3.2") "base"))
 (define build-deps
-  '("graph-lib" "rackunit-lib" "racket-doc" "scribble-lib" "data-doc" "math-doc"))
+  '("graph-lib" "math-lib" "rackunit-lib" "racket-doc" "scribble-lib" "data-doc" "math-doc"))
 
 (define update-implies '("graph-lib"))
