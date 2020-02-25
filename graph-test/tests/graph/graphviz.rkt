fb3c144665f2384f8b5013f9472e688aca8e9674
Sergiu Ivanov <scolobb@colimite.fr>
2020-02-25 23:10:13 +0100

    Explicitly allow non-numerical edges (#54)
    * graphviz: Use equal? instead of = to compare edge labels.
    
    Also add some tests for arbitrary labels and Graphviz rendering.
    
    * graphviz.rkt: Don't use brackets [ ] in tests with string-contains?.
    
    The square brackets introduce a character class, which means that
    "[color=\"0.0 1.0 1.0\"]" matches something completely different from
    "color=\"0.0 1.0 1.0\"".  In particular, in these tests the bracketed
    strings matched too many patterns.
    
    * graph.scrbl: Say that edge weights can be any value comparable with equal?.
    
    Also say that some algorithms expect numerical edge weights.
    
    closes #54

--- graph-test/tests/graph/graphviz.rkt
+++ graph-test/tests/graph/graphviz.rkt
@@ -57,18 +57,85 @@
 ;; 	subgraph D {
 ;; 	}
 ;; }
-(check-true (string-contains? bf-viz-color "[color=\"0.0 1.0 1.0\"]"))
-(check-true (string-contains? bf-viz-color "[color=\"0.333 1.0 1.0\"]"))
-(check-true (string-contains? bf-viz-color "[color=\"0.666 1.0 1.0\"]"))
-(check-true (string-contains? bf-viz-color "[color=\"0.0 1.0 1.0\"]"))
-(check-true (string-contains? bf-viz-color "[color=\"0.333 1.0 1.0\"]"))
+(check-true (string-contains? bf-viz-color "color=\"0.0 1.0 1.0\""))
+(check-true (string-contains? bf-viz-color "color=\"0.333 1.0 1.0\""))
+(check-true (string-contains? bf-viz-color "color=\"0.666 1.0 1.0\""))
+(check-true (string-contains? bf-viz-color "color=\"0.0 1.0 1.0\""))
+(check-true (string-contains? bf-viz-color "color=\"0.333 1.0 1.0\""))
 (check-butterfly-viz-output bf-viz-color)
 
 (define bf-viz-color-brelaz 
   (graphviz butterfly #:colors (coloring/brelaz butterfly)))
-(check-true (string-contains? bf-viz-color-brelaz "[color=\"0.333 1.0 1.0\"]"))
-(check-true (string-contains? bf-viz-color-brelaz "[color=\"0.666 1.0 1.0\"]"))
-(check-true (string-contains? bf-viz-color-brelaz "[color=\"0.0 1.0 1.0\"]"))
-(check-true (string-contains? bf-viz-color-brelaz "[color=\"0.333 1.0 1.0\"]"))
-(check-true (string-contains? bf-viz-color-brelaz "[color=\"0.666 1.0 1.0\"]"))
+(check-true (string-contains? bf-viz-color-brelaz "color=\"0.333 1.0 1.0\""))
+(check-true (string-contains? bf-viz-color-brelaz "color=\"0.666 1.0 1.0\""))
+(check-true (string-contains? bf-viz-color-brelaz "color=\"0.0 1.0 1.0\""))
+(check-true (string-contains? bf-viz-color-brelaz "color=\"0.333 1.0 1.0\""))
+(check-true (string-contains? bf-viz-color-brelaz "color=\"0.666 1.0 1.0\""))
 (check-butterfly-viz-output bf-viz-color-brelaz)
+
+(define fruit-labeled
+  (weighted-graph/directed '(["tastier" "tomato" "potato"]
+                             [10 "apple" "tomato"]
+                             [11 "apple" "potato"]
+                             [better "banana" "apple" ])))
+(check-equal? (edge-weight fruit-labeled "tomato" "potato") "tastier")
+(check-equal? (edge-weight fruit-labeled "apple" "tomato") 10)
+(check-equal? (edge-weight fruit-labeled "apple" "potato") 11)
+(check-equal? (edge-weight fruit-labeled "banana" "apple") 'better)
+
+(define fruit-labeled-viz (graphviz fruit-labeled))
+;; eg,
+;; digraph G {
+;; 	node0 [label="banana"];
+;; 	node1 [label="tomato"];
+;; 	node2 [label="apple"];
+;; 	node3 [label="potato"];
+;; 	subgraph U {
+;; 		edge [dir=none];
+;; 	}
+;; 	subgraph D {
+;; 		node0 -> node2 [label="better"];
+;; 		node1 -> node3 [label="tastier"];
+;; 		node2 -> node1 [label="10"];
+;; 		node2 -> node3 [label="11"];
+;; 	}
+;; }
+(check-true (string-contains? fruit-labeled-viz "\\[label=\"better\"\\]"))
+(check-true (string-contains? fruit-labeled-viz "\\[label=\"tastier\"\\]"))
+(check-true (string-contains? fruit-labeled-viz "\\[label=\"10\"\\]"))
+(check-true (string-contains? fruit-labeled-viz "\\[label=\"11\"\\]"))
+
+(define cities-labeled
+  (weighted-graph/undirected '([far "Berlin" "New York"]
+                               ["near" "New York" "Boston"]
+                               [1000 "Berlin" "Boston"]
+                               ["very far" "New York" "Sydney"])))
+(define (check-undirected-edge-label-equal? gr u v label)
+  (check-equal? (edge-weight gr u v) label)
+  (check-equal? (edge-weight gr v u) label))
+(check-undirected-edge-label-equal? cities-labeled "Berlin" "New York" 'far)
+(check-undirected-edge-label-equal? cities-labeled "New York" "Boston" "near")
+(check-undirected-edge-label-equal? cities-labeled "Berlin" "Boston" 1000)
+(check-undirected-edge-label-equal? cities-labeled "New York" "Sydney" "very far")
+
+(define cities-labeled-viz (graphviz cities-labeled))
+;; eg,
+;; digraph G {
+;; 	node0 [label="Sydney"];
+;; 	node1 [label="Berlin"];
+;; 	node2 [label="New York"];
+;; 	node3 [label="Boston"];
+;; 	subgraph U {
+;; 		edge [dir=none];
+;; 		node0 -> node2 [label="very far"];
+;; 		node1 -> node3 [label="1000"];
+;; 		node1 -> node2 [label="far"];
+;; 		node2 -> node3 [label="near"];
+;; 	}
+;; 	subgraph D {
+;; 	}
+;; }
+(check-true (string-contains? cities-labeled-viz "\\[label=\"very far\"\\]"))
+(check-true (string-contains? cities-labeled-viz "\\[label=\"1000\"\\]"))
+(check-true (string-contains? cities-labeled-viz "\\[label=\"far\"\\]"))
+(check-true (string-contains? cities-labeled-viz "\\[label=\"near\"\\]"))
