d5d4c34eddb5061d3d72ff395950dbbce136860f
Stephen Chang <stephen.chang@umb.edu>
2021-10-20 17:50:50 -0400

    bump timeout of timing tests

--- graph-test/tests/info.rkt
+++ graph-test/tests/info.rkt
@@ -3,5 +3,5 @@
 (define compile-omit-paths '("graph"))
 
 (define test-timeouts
-  '(("graph/timing-test-in-neighbors.rkt" 180)
-    ("graph/timing-test-scc.rkt" 180)))
+  '(("graph/timing-test-in-neighbors.rkt" 360)
+    ("graph/timing-test-scc.rkt" 360)))
